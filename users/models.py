19deaa2fc7a6b6c240e2c90a97c7503ca3277592
Paul Huang <paul_hzq@hotmail.com>
2017-11-13 18:17:45 -0500

    add the models to boards

--- users/models.py
+++ users/models.py
@@ -1,4 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class UserManager():
+    '''https://github.com/django/django/blob/master/django/contrib/auth/models.py#L131'''
     pass
+
+class User(AbstractUser):
+    '''https://github.com/django/django/blob/master/django/contrib/auth/models.py#L288'''
+    object = UserManager()
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
