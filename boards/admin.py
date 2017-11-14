03cf9e8efe86a2480f33b4bc4df42dd7861c65d7
Paul Huang <paul_hzq@hotmail.com>
2017-11-13 21:50:30 -0500

    register model

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Board, Topic, Post
 # Register your models here.
+
+# @admin.register()
+# class
+admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Post)
