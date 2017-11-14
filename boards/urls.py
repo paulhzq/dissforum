5915abaccdf9ebeee3fb14abe7bb3f0529cd8668
Paul Huang <paul_hzq@hotmail.com>
2017-11-14 14:09:23 -0500

    create form for new topic

--- boards/urls.py
+++ boards/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import home, board_topics
+from .views import home, board_topics, new_topic
 
 urlpatterns = [
     url(r'^$', home, name ='home'),
     url(r'boards/(?P<pk>\d+)/$',board_topics, name = 'board_topics'),
+    url(r'boards/(?P<pk>\d+)/new/$', new_topic, name = 'new_topic'),
 ]
