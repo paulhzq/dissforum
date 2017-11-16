25316599dcd0bd15e5e184f2bf5aa733ca3d2847
Paul Huang <paul_hzq@hotmail.com>
2017-11-16 10:26:50 -0500

    move the settings to env file

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY');
+SECRET_KEY = config('SECRET_KEY');
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,9 +87,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': '127.0.0.1',
         'PORT': 5432,
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,8 +1,14 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as basic_userManager
 
-class UserManager():
+
+class UserManager(basic_userManager):
     '''https://github.com/django/django/blob/master/django/contrib/auth/models.py#L131'''
-    pass
+    def _create_user(self, username, email, password, **extra_fields):
+        return super()._create_user(usrname, email,password, **extra_fields)
+
+    def create_user(self,username, email=None, password=None, **extra_fields):
+        super().create_user(username,email,password,**extra_fields)
+
 
 class User(AbstractUser):
     '''https://github.com/django/django/blob/master/django/contrib/auth/models.py#L288'''
